4bacd078af128260032506dd3f1c7b7e9cfbe436
Bart Alcorn <bart.alcorn@turner.com>
2021-09-21 07:08:39 -0400

    added lambda-event.tf

---
+++ pkg/efs/templates/lambda-event.tf
@@ -0,0 +1,39 @@
+### EVENT Lambda ====================================================================
+module "lambdaEVENT" {
+  source               = "github.com/turnercode/cp-awfm-iac/modules/aws_lambda_container"
+  constants            = module.constants
+  app                  = var.app
+  environment          = var.environment
+  lambda_bucket_name   = module.constants.lambda_bucket_name
+  lambda_function_name = "${var.environment}-${var.app}-event"
+  image_uri            = "${module.constants.aws_account_id}.dkr.ecr.${module.constants.aws_region}.amazonaws.com/hello-world-api:0.0.1"
+  handler              = "/var/task/${var.app}"
+  lambda_policy        = data.aws_iam_policy_document.lambda_event_permissions.json
+  lambda_timeout       = 30
+  description          = "example '${var.app}' container deployment"
+  tags                 = merge(module.constants.tags, { environment = var.environment })
+}
+
+data "aws_iam_policy_document" "lambda_event_permissions" {
+  statement {
+    # sid       = "AllowInvokingLambdas"
+    effect    = "Allow"
+    resources = ["arn:aws:lambda:us-east-1:026155191598:function:${var.environment}-${var.app}-event:*"]
+    actions   = ["lambda:*"]
+  }
+
+  statement {
+    # sid       = "AllowSecretsManager"
+    effect    = "Allow"
+    resources = ["arn:aws:secretsmanager:us-east-1:026155191598:secret:zephyr/configs/*"]
+    actions = [
+      "secretsmanager:GetRandomPassword",
+      "secretsmanager:ListSecrets",
+      "secretsmanager:GetResourcePolicy",
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecretVersionIds",
+    ]
+  }
+
+}
